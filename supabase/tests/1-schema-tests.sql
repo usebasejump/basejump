91732f0a68ea17e7a5f8bf16591ef544c55b46b3
tiniscule <114822071+tiniscule@users.noreply.github.com>
2022-10-17 23:39:51 -0400

    clean up some tests

--- supabase/tests/1-schema-tests.sql
+++ supabase/tests/1-schema-tests.sql
@@ -9,12 +9,12 @@ select has_schema('basejump', 'Basejump schema should exist');
 
 select has_table('basejump', 'config', 'Basejump config table should exist');
 
-select columns_are('basejump', 'config', Array['enable_personal_accounts', 'enable_team_accounts', 'enable_account_billing', 'stripe_default_trial_period_days', 'stripe_default_account_price_id'], 'Basejump config table should have the correct columns');
+select columns_are('basejump', 'config', Array['enable_personal_accounts', 'enable_team_accounts', 'enable_account_billing', 'billing_provider', stripe_default_trial_period_days', 'stripe_default_account_price_id'], 'Basejump config table should have the correct columns');
 
 select ok(basejump.is_set('enable_personal_accounts')), 'Basejump config should have personal accounts enabled';
 select ok(basejump.is_set('enable_team_accounts')), 'Basejump config should have team accounts enabled';
 select ok(basejump.is_set('enable_account_billing')), 'Basejump config should have account billing enabled';
-select ok(basejump.get_config() -> 'stripe_default_trial_period_days' = 30), 'Basejump config should have a default trial period';
+select ok((basejump.get_config() -> 'stripe_default_trial_period_days')::int = 30), 'Basejump config should have a default trial period';
 
 SELECT * FROM finish();
 
