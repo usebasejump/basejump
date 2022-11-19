9363acdeb56e2b051919a1df566b68af35422fc0
tiniscule <114822071+tiniscule@users.noreply.github.com>
2022-11-19 11:43:52 -0600

    Switch to middleware for invitations (#22)

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,31 @@
-import { withMiddlewareAuth } from "@supabase/auth-helpers-nextjs";
-import { LOGIN_PATH } from "@/types/auth";
+import { createMiddlewareSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
 
-export const middleware = withMiddlewareAuth({ redirectTo: LOGIN_PATH });
+export async function middleware(req: NextRequest) {
+  // We need to create a response and hand it to the supabase client to be able to modify the response headers.
+  const res = NextResponse.next();
+
+  const supabase = await createMiddlewareSupabaseClient({ req, res });
+
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  // Check auth condition
+  if (session) {
+    // Authentication successful, forward request to protected route.
+    return res;
+  }
+
+  // Auth condition not met, redirect to home page.
+  const redirectUrl = req.nextUrl.clone();
+  redirectUrl.pathname =
+    req.nextUrl.pathname === "/invitation" ? "/signup" : "/login";
+  redirectUrl.searchParams.set(`redirectedFrom`, req.url);
+  return NextResponse.redirect(redirectUrl);
+}
 
 export const config = {
-  matcher: ["/dashboard/:path*"],
+  matcher: ["/dashboard/:path*", "/invitation"],
 };
---
+++ src/utils/use-auth-check.ts
@@ -0,0 +1,23 @@
+import { useRouter } from "next/router";
+import { useSessionContext } from "@supabase/auth-helpers-react";
+import { useEffect } from "react";
+
+/**
+ * There's an issue with Supabase Auth where oauth and magic links don't work with middleware
+ * on the initial page load.  This is because the token is sent over a hash param which
+ * is not sent to the server. This hook sits client side, checks for an authenticated session on the login
+ * page, and redirects the user if it's found.
+ * @param redirectedFrom
+ */
+const useAuthCheck = (redirectedFrom?: string) => {
+  const { replace } = useRouter();
+  const { session } = useSessionContext();
+
+  useEffect(() => {
+    if (session && redirectedFrom) {
+      replace(redirectedFrom as string);
+    }
+  }, [session, redirectedFrom]);
+};
+
+export default useAuthCheck;
